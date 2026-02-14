63816e1d9aec643f6a57b1116e867cbe0ba1ae2b
Phil McAuliffe <mcflip@gmail.com>
2026-02-14 22:21:40 +1100

    Fix withOpacity deprecated replace with .withValues()

--- lib/head_to_head_screen.dart
+++ lib/head_to_head_screen.dart
@@ -105,7 +105,7 @@ class _HeadToHeadScreenState extends State<HeadToHeadScreen> {
             borderRadius: BorderRadius.circular(20),
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.05),
+                color: Colors.black.withValues(alpha: 0.05),
                 blurRadius: 10,
                 offset: const Offset(0, 5),
               )
